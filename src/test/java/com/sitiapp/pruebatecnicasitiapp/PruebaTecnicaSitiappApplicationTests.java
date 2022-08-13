c9844a6aa6254f241e2ea7036261aaa76396784a
fdcaballero <fredycaballero034@gmail.com>
2022-08-12 21:59:36 -0500

    :tada: init

---
+++ src/test/java/com/sitiapp/pruebatecnicasitiapp/PruebaTecnicaSitiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitiapp.pruebatecnicasitiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSitiappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
